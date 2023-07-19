ff46a2d865a99f5f96ce8a274628f099f955b291
Ibn Lisha <ibn.lisha@yahoo.com>
2023-07-19 17:01:27 +0300

    first commit

---
+++ src/components/About/index.tsx
@@ -0,0 +1,13 @@
+import './about.css'
+
+const About = () => (<div className="about-container container">
+    <div className="profile">
+        <h1>Hello! I am Ibrahim</h1>
+        <h2>Full stack developer</h2>
+    </div>
+    <div className="frame">
+        <img src="images/profile-no-bg.png" alt="profile" className="profileImage"/>
+    </div>
+</div>)
+
+export default About
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,15 @@
+import './navbar.css';
+
+const Navbar = () =>(<div className ='navbar'>
+    <div className="container justify-content-space">
+    <h1 className="NavbarBrand">Ibralisha</h1>
+    <nav className="nav">
+        <li className="nav-link">About</li>
+        <li className="nav-link">Prejects</li>
+        <li className="nav-link">Skills</li>
+        <li className="nav-link">Contact</li>
+    </nav>
+    </div>
+</div>)
+
+export default Navbar
